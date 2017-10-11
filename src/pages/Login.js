82bdbe1296cd330ea917087078ab3cf700029883
WJH <hou32hou@gmail.com>
2017-10-12 00:30:17 +0800

    Added Login.css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,43 +3,38 @@ import Center from 'react-center';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Card from 'material-ui/Card';
-
-const Style_Card = {
-    padding: 15
-}
-
-const Style_Field = {
-    margin: "0",
-    fontSize: 20,
-    fontWeight : "bold"
-}
-
-const Style_Button = {
-    margin: "12,12,12,0",   
-    fontSize: 20,
-    fontWeight : "bold"
-}
+import './Login.css'
 
 export function Login() {
     return (
         <Center>
-            <div className="hello">
-                <Card className="card" style={Style_Card}>
+            <div className="login-div">
+                <Card className="login-card">
                     <TextField
+                        className="login-student-id-field"
                         floatingLabelText="Student ID"
-                        hintText="e.g. 1500181"
-                        style={Style_Field}/>
+                        hintText="e.g. 1500181"/>
                     <br/>
                     <TextField
+                        className="login-password-field"
                         hintText="e.g. 960707-43-1234"
                         floatingLabelText="Password"
-                        type="password"
-                        style={Style_Field}
-                        />
+                        type="password"/>
                     <br/>
-                    <img src="https://unitreg.utar.edu.my/portal/Kaptcha.jpg"/>
+                    <TextField
+                        className="login-kapcha-field"
+                        hintText="e.g. QXtresZ"
+                        floatingLabelText="Kaptcha"/>
                     <br/>
-                    <RaisedButton label="Login" primary={true} style={Style_Button} fullWidth={true}/>
+                    <img
+                        className="login-kapcha-image"
+                        src="https://unitreg.utar.edu.my/portal/Kaptcha.jpg"/>
+                    <br/>
+                    <RaisedButton
+                        className="login-button"
+                        label="Login"
+                        primary={true}
+                        fullWidth={true}/>
                 </Card>
             </div>
         </Center>
