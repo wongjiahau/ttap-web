78d923e4ce1709cb9b70c4d945918c98e5abf2f2
WJH <hou32hou@gmail.com>
2017-10-11 21:08:50 +0800

    Uses Material-UI at App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {MuiThemeProvider} from 'material-ui'
+import {Login} from './pages/Login'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro"></p>
+          <Login/>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
-
-export default App;
