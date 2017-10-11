bae54d7fba4a2e8e3fd0a854240e4b7cddedb690
WJH <hou32hou@gmail.com>
2017-10-11 23:44:51 +0800

    Added app header in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {MuiThemeProvider} from 'material-ui'
 import {Login} from './pages/Login'
+import AppBar from 'material-ui/AppBar'
 
 export default class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro"></p>
-          <Login/>
-        </div>
+        <AppBar
+          className="App-header"
+          title="Timetable Arranging Program (UTAR)"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+        <br/>
+        <Login/>
       </MuiThemeProvider>
     )
   }
