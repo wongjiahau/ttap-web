0d499462f1c7a28ced9e2a50d315459b112ac0cd
WJH <hou32hou@gmail.com>
2017-10-12 12:39:52 +0800

    Added link for menu item in Drawer

--- src/DrawerContent.js
+++ src/DrawerContent.js
@@ -1,15 +1,19 @@
+import BugIcon from 'material-ui-icons/BugReport';
+import FeedbackIcon from 'material-ui-icons/Feedback';
+import HelpIcon from 'material-ui-icons/Help';
+import InfoIcon from 'material-ui-icons/Info';
 import MenuItem from 'material-ui/MenuItem';
 import React from 'react';
-import InfoIcon from 'material-ui-icons/Info';
-import FeedbackIcon from 'material-ui-icons/Feedback';
-import BugIcon from 'material-ui-icons/BugReport';
-import Help from 'material-ui-icons/Help';
 
 const MenuItemStyle = {
     fontSize: 15,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    target: '_blank'
 };
 
+const FeedbackFormUrl = "https://goo.gl/forms/qKdc6EVGbxspoTaS2";
+const ReportBugUrl = "https://goo.gl/forms/4PJupNgRTEyGGTCN2";
+const ReadMeUrl = "https://github.com/wongjiahau/TTAP-UTAR/blob/master/README.md";
 class DrawerContent extends React.Component {
     constructor() {
         super();
@@ -20,13 +24,13 @@ class DrawerContent extends React.Component {
 
     render() {
         return <div>
-            <MenuItem style={MenuItemStyle} rightIcon={< InfoIcon />}>Help</MenuItem>
-            <MenuItem style={MenuItemStyle} rightIcon={< FeedbackIcon />}>Feedback</MenuItem>
-            <MenuItem style={MenuItemStyle} rightIcon={< BugIcon />}>Report a bug</MenuItem>
-            <MenuItem style={MenuItemStyle} rightIcon={< Help />}>About TTAP</MenuItem>
+            <MenuItem target='_blank' style={MenuItemStyle} rightIcon={< InfoIcon />} href={ReadMeUrl}>Help</MenuItem>
+            <MenuItem target='_blank' style={MenuItemStyle} rightIcon={< FeedbackIcon />} href={FeedbackFormUrl}>Feedback</MenuItem>
+            <MenuItem target='_blank' style={MenuItemStyle} rightIcon={< BugIcon />} href={ReportBugUrl}>Report a bug</MenuItem>
+            <MenuItem target='_blank' style={MenuItemStyle} rightIcon={< HelpIcon />}>About TTAP</MenuItem>
         </div>;
     }
-
+    //TODO: Add a link for About TTAP
     componentDidMount() {
         this.setState({someKey: 'otherValue'});
     }
