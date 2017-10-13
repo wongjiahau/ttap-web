21a44a1e2ccfeb593c463c0af1acf67e9a5a633b
unknown <dheenodararao@gmail.com>
2017-10-13 16:11:29 +0800

    Completed Test 3 and Test 4

--- ts/att/_time.test.ts
+++ ts/att/_time.test.ts
@@ -19,4 +19,16 @@ describe("time", () => {
         const input2 = Time.CreateTime24Hour(11, 10);
         expect(input1.Equal(input2)).to.equal(false);
     });
+
+    it("Test Equal() 3", () => {
+        const input1 = Time.CreateTime24Hour(10, 10);
+        const input2 = Time.CreateTime24Hour(10, 11);
+        expect(input1.Equal(input2)).to.equal(false);
+    });
+
+    it("Test Equal() 4", () => {
+        const input1 = Time.CreateTime24Hour(10, 10);
+        const input2 = Time.CreateTime24Hour(11, 11);
+        expect(input1.Equal(input2)).to.equal(false);
+    });
 });
