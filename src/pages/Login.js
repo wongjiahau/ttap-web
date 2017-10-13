34072624f25eeeeed1e926d7fa87351201ad1ca3
WJH <hou32hou@gmail.com>
2017-10-13 15:31:51 +0800

    Finally can pass data from login UI to the course website

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import Center from 'react-center';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
@@ -54,55 +54,94 @@ const style = {
     },
     header1: {
         fontSize: '72px'
+    },
+    iframe: {
+        width: 1,
+        height: 1,
+        border: 0
     }
-
 }
 //TODO: Add the link for TOS and PP
-export default function Login() {
-    return (
-        <div>
-            <div style={style.cover}/>
-            <div style={style.headerDiv}>
-                <h1 style={style.header1}>Login to your
-                    <br/>UTAR account</h1>
-                <div style={style.subHeaderDiv}>
-                    By clicking "Login", you agree to our &nbsp;
-                    <a href='https://www.google.com' target='_blank'>Terms of Service and Privacy Policy</a>
+export default class Login extends Component {
+    handleClick = () => {
+        var iframeDoc = document
+            .getElementById('iframe')
+            .contentDocument;
+        iframeDoc.getElementsByName('reqFregkey')[0].value = document
+            .getElementById('student-id-field')
+            .value;
+        iframeDoc.getElementsByName('reqPassword')[0].value = document
+            .getElementById('password-field')
+            .value;
+        iframeDoc.getElementsByName('kaptchafield')[0].value = document
+            .getElementById('kapcha-field')
+            .value;
+        iframeDoc
+            .getElementsByName('_submit')[0]
+            .click();
+        alert(document.getElementById('iframe').contentWindow.location.href);
+    }
+
+    loadKapchaImage = () => {
+        document
+            .getElementById('kapcha-img')
+            .src = 'https://unitreg.utar.edu.my/portal/Kaptcha.jpg';
+        alert(document.getElementById('iframe').contentWindow.location.href);
+    }
+
+    render() {
+        return (
+            <div id='parentDiv'>
+                <div style={style.cover}/>
+                <div style={style.headerDiv}>
+                    <h1 style={style.header1}>Login to your
+                        <br/>UTAR account</h1>
+                    <div style={style.subHeaderDiv}>
+                        By clicking "Login", you agree to our &nbsp;
+                        <a href='https://www.google.com' target='_blank'>Terms of Service and Privacy Policy</a>
+                    </div>
+                </div>
+                <div className="login-div">
+                    <Paper style={style.card} zDepth={4}>
+                        <TextField
+                            id='student-id-field'
+                            className="login-student-id-field"
+                            floatingLabelText="Student ID"
+                            style={style.field}
+                            hintText="e.g. 1500181"/>
+                        <br/>
+                        <TextField
+                            id='password-field'
+                            className="login-password-field"
+                            floatingLabelText="Password"
+                            hintText="e.g. 960707-43-1234"
+                            style={style.field}
+                            type="password"/>
+                        <br/>
+                        <TextField
+                            id='kapcha-field'
+                            hintText="e.g. QXtresZ"
+                            style={style.field}
+                            floatingLabelText="Kaptcha"/>
+                        <br/>
+                        <img id='kapcha-img' style={style.kapcha} className="login-kapcha-image"/>
+                        <br/>
+                        <RaisedButton
+                            className="login-button"
+                            label="Login"
+                            primary={true}
+                            onClick={this.handleClick}
+                            style={style.button}/>
+                        <iframe
+                            id='iframe'
+                            onLoad={this.loadKapchaImage}
+                            style={style.iframe}
+                            src="https://unitreg.utar.edu.my/portal/courseRegStu/login.jsp">
+                            <p>Your browser does not support iframes.</p>
+                        </iframe>
+                    </Paper>
                 </div>
             </div>
-            <div className="login-div">
-                <Paper style={style.card} zDepth={3}>
-                    <TextField
-                        className="login-student-id-field"
-                        floatingLabelText="Student ID"
-                        style={style.field}
-                        hintText="e.g. 1500181"/>
-                    <br/>
-                    <TextField
-                        className="login-password-field"
-                        floatingLabelText="Password"
-                        hintText="e.g. 960707-43-1234"
-                        style={style.field}
-                        type="password"/>
-                    <br/>
-                    <TextField
-                        className="login-kapcha-field"
-                        hintText="e.g. QXtresZ"
-                        style={style.field}
-                        floatingLabelText="Kaptcha"/>
-                    <br/>
-                    <img
-                        style={style.kapcha}
-                        className="login-kapcha-image"
-                        src="https://unitreg.utar.edu.my/portal/Kaptcha.jpg"/>
-                    <br/>
-                    <RaisedButton
-                        className="login-button"
-                        label="Login"
-                        primary={true}
-                        style={style.button}/>
-                </Paper>
-            </div>
-        </div>
-    );
+        );
+    }
 }
