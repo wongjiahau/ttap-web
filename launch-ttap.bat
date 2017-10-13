e9a5d098552ef5d79f85b27a2dc59dc76a980f0e
WJH <hou32hou@gmail.com>
2017-10-13 18:26:36 +0800

    Updated launch-ttap.bat to use the correct website

--- launch-ttap.bat
+++ launch-ttap.bat
@@ -1,1 +1,1 @@
-start chrome.exe --user-data-dir="C:/chrome-dev-session" --disable-web-security --app=localhost:3000
+start chrome.exe --user-data-dir="C:/chrome-dev-session" --disable-web-security --app=https://wongjiahau.github.io/ttap-web/
