8d9b9688eb46da746e5747bc54b6c6a9c942763a
unknown <dheenodararao@gmail.com>
2017-10-14 13:57:33 +0800

    Completed equal 5 and less than 1 to less than 6

--- ts/att/time.ts
+++ ts/att/time.ts
@@ -4,6 +4,14 @@ export class Time {
         return new Time(hour, minute);
     }
 
+    public static CreateTime12Hour(hour: number, minute: number, isPm: boolean): Time {
+        if (hour < 1 || hour > 12) {throw new RangeError("Expected value is between 1 to 12, but actual is {hour}"); }
+        if (isPm && hour !== 12) {hour += 12; }
+        if (!isPm && hour === 12) {hour = 0; }
+        const t = Time.CreateTime24Hour(hour, minute);
+        return t;
+    }
+
     public Hour: number;
     public Minute: number;
 
@@ -11,6 +19,7 @@ export class Time {
         this.Hour = hour;
         this.Minute = minute;
     }
+
     public Equal(other: Time): boolean {
         if (this.Hour !== other.Hour) {
             return false;
@@ -19,5 +28,12 @@ export class Time {
             return false;
         }
         return true;
+    }
+
+    public LessThan(other: Time): boolean {
+        if (this.Hour < other.Hour) {return true; }
+        if (this.Hour > other.Hour) {return false; }
+        if (this.Minute < other.Minute) {return true; }
+        return false;
     }
 }
