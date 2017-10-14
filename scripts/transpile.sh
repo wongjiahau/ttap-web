62eebd99719e8b8d8d117975f1e5363eb5b9b1c1
WJH <hou32hou@gmail.com>
2017-10-14 15:11:08 +0800

    Updated tasks.json to use transpile.sh as build task

--- scripts/transpile.sh
+++ scripts/transpile.sh
@@ -3,3 +3,7 @@ rm -r src/core*
 
 #This is the script to transpile TS into JS
 tsc -p ./ts/tsconfig.json
+
+# Echo completion
+echo "Successfully transpiled TS into JS"
+echo "The output files are located at src/core"
