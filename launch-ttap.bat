18dc5ee1ccf467041264fabdac5c32a35bd927b5
WJH <hou32hou@gmail.com>
2017-10-14 08:55:41 +0800

    Added --allow-running-insecure-content for launch-ttap.bat -so that HTTP content can be loaded over HTTPS

--- launch-ttap.bat
+++ launch-ttap.bat
@@ -1,1 +1,1 @@
-start chrome.exe --user-data-dir="C:/chrome-dev-session" --disable-web-security -incognito --app=https://wongjiahau.github.io/ttap-web/
+start chrome.exe --user-data-dir="C:/chrome-dev-session" --disable-web-security --allow-running-insecure-content -incognito --app=https://wongjiahau.github.io/ttap-web/
