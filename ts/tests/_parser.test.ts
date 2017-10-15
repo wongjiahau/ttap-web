d393964159263d77ebef9b1af906bf099f6c332d
WJH <hou32hou@gmail.com>
2017-10-15 22:52:15 +0800

    Completed 90% code for parser

--- ts/tests/_parser.test.ts
+++ ts/tests/_parser.test.ts
@@ -1,4 +1,3 @@
-
 import {assert, expect} from "chai";
 import * as S from "string";
 import testManager from "./testManager";
@@ -6,10 +5,31 @@ import {FileName} from "./testManager";
 
 describe("Parser which is used to parse html into slots", () => {
     it("GetFileNameOf should return string based on FileName enum", () => {
-        const result = new testManager().GetDataFrom(FileName.jiahau_2017_sept);
-        expect(S(result)
-            .contains("unitreg"))
-            .to
-            .equal(true);
+        const plainHtml = new testManager().GetDataFrom(FileName.jiahau_2017_sept);
+        const htmlDoc = new DOMParser().parseFromString(plainHtml, "text/html");
+        const tableRows = htmlDoc
+            .getElementById("overviewSector")
+            .getElementsByTagName("table")[0]
+            .getElementsByTagName("tbody")[0]
+            .getElementsByTagName("tr");
+        // i = 1 because we need to skip the first <tr> which is the header of the table
+        for (let i = 1; i < tableRows.length; i++) {
+            const cells = tableRows[i].getElementsByTagName("td");
+            for (let j = 0; j < cells.length; j++) {
+                switch (j) {
+                    case 0: console.log("number = " + cells[j].innerHTML); break;
+                    case 1: console.log("type   = " + cells[j].innerHTML); break;
+                    case 2: console.log("group  = " + cells[j].innerHTML); break;
+                    case 3: console.log("Size   = " + cells[j].innerHTML); break;
+                    case 4: console.log("day    = " + cells[j].innerHTML); break;
+                    case 5: console.log("time   = " + cells[j].innerHTML); break;
+                    case 6: console.log("hour   = " + cells[j].innerHTML); break;
+                    case 7: console.log("week   = " + cells[j].innerHTML); break;
+                    case 8: console.log("room   = " + cells[j].innerHTML); break;
+                    case 9: console.log("remark = " + cells[j].innerHTML); break;
+                }
+            }
+        }
+
     });
 });
