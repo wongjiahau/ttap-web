7475c66706edc94e7f138784d08580bdd4cce851
WJH <hou32hou@gmail.com>
2017-10-15 13:24:08 +0800

    Passed test for App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import RaisedButton from 'material-ui/RaisedButton';
 import React, {Component} from 'react';
 import {MuiThemeProvider} from 'material-ui';
+import {HashRouter as Router} from 'react-router-dom'
 
 export default class App extends Component {
   constructor(props) {
@@ -21,32 +22,34 @@ export default class App extends Component {
   handleClose = () => this.setState({isSecondaryDrawerOpened: false});
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <AppBar
-            className="App-header"
-            title="Timetable Arranging Program (UTAR)"
-            iconElementLeft={< p > </p>}
-            iconElementRight={< IconButton onClick = {
-            this.handleOpen
-          } > <MenuIcon/> </IconButton>}/>
-          <br/>
-          <Drawer
-            width={200}
-            docked={false}
-            openSecondary={true}
-            open={this.state.isSecondaryDrawerOpened}>
-            <RaisedButton
-              label='hide drawer'
-              fullWidth={true}
-              secondary={true}
-              icon={< ArrowForward />}
-              onClick={this.handleClose}/>
-            <DrawerContent/>
-          </Drawer>
-          <Main/>
-        </div>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              className="App-header"
+              title="Timetable Arranging Program (UTAR)"
+              iconElementLeft={< p > </p>}
+              iconElementRight={< IconButton onClick = {
+              this.handleOpen
+            } > <MenuIcon/> </IconButton>}/>
+            <br/>
+            <Drawer
+              width={200}
+              docked={false}
+              openSecondary={true}
+              open={this.state.isSecondaryDrawerOpened}>
+              <RaisedButton
+                label='hide drawer'
+                fullWidth={true}
+                secondary={true}
+                icon={< ArrowForward />}
+                onClick={this.handleClose}/>
+              <DrawerContent/>
+            </Drawer>
+            <Main/>
+          </div>
+        </MuiThemeProvider>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'typeface-roboto';
 
 ReactDOM.render(
-    <Router >
     <App/>
-</Router>, document.getElementById('root'));
+, document.getElementById('root'));
 registerServiceWorker();
