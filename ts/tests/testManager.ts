76301579107ede3114eb59eb805c379387a4a793
WJH <hou32hou@gmail.com>
2017-10-16 13:09:44 +0800

    Added new test data

--- ts/tests/testManager.ts
+++ ts/tests/testManager.ts
@@ -16,4 +16,7 @@ function LoadLocalFile(fileName: string): string {
 
 export enum FileName {
     jiahau_2017_sept = "jiahau_2017_sept.html",
+    heng_2017_sept = "heng_2017_sept.html",
+    keli_2017_sept = "keli_2017_sept.html",
+    qingsheng_2017_sept = "qingsheng_2017_sept.html",
 }
