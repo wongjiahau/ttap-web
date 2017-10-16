44e89e5e35f4104e683ab802a88e87a78feb8772
WJH <hou32hou@gmail.com>
2017-10-16 08:55:43 +0800

    Renamed slot to rawSlot Because it is storing string-only attributes, therefore we need another function to convert rawSlot into Slot which are comparable.

---
+++ ts/model/rawSlot.ts
@@ -0,0 +1,38 @@
+export default class RawSlot {
+    private static hash = 0;
+    public readonly HashId: number; // Unique for every slot object
+    public SubjectCode: string;
+    public SubjectName: string;
+    public Number: string;
+    public Type: string;
+    public Group: string;
+    public ClassSize: string;
+    public Day: string;
+    public TimePeriod: string;
+    public CreditHour: string;
+    public WeekNumber: string;
+    public Remark: string;
+    public Room: string;
+    constructor() {
+        this.HashId = RawSlot.hash;
+        RawSlot.hash++;
+
+    }
+    public toString = (): string => {
+        return `---
+    SubjectCode : ${this.SubjectCode}
+    SubjectName : ${this.SubjectName}
+    Number      : ${this.Number}
+    Type        : ${this.Type}
+    Group       : ${this.Group}
+    ClassSize   : ${this.ClassSize}
+    Day         : ${this.Day}
+    TimePeriod  : ${this.TimePeriod}
+    CreditHour  : ${this.CreditHour}
+    WeekNumber  : ${this.WeekNumber}
+    Remark      : ${this.Remark}
+    Room        : ${this.Room}
+`;
+
+    }
+}
