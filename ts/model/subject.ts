750e2b9e02ae78396f5f3aa983f493a1c94a5b73
WJH <hou32hou@gmail.com>
2017-10-17 23:18:18 +0800

    Now subject will store slot IDs instead of slots

--- ts/model/subject.ts
+++ ts/model/subject.ts
@@ -2,10 +2,10 @@ import Slot from "./rawSlot";
 export default class Subject {
     public readonly Name: string;
     public readonly Code: string;
-    public readonly Slots: Slot[];
-    constructor(name: string, code: string, slots: Slot[]) {
+    public readonly SlotIds: number[];
+    constructor(name: string, code: string, slotIds: number[]) {
         this.Name = name;
         this.Code = code;
-        this.Slots = slots;
+        this.SlotIds = slotIds;
     }
 }
