cc1498e3cdb27a81e23ef41183a600cf0decb1a7
WJH <hou32hou@gmail.com>
2017-10-19 22:24:43 +0800

    Updated UI of subjectListView

--- ts/react/subjectListView.tsx
+++ ts/react/subjectListView.tsx
@@ -1,7 +1,15 @@
+import Paper from "material-ui/Paper";
 import * as React from "react";
 import {Beautify, GetInitial} from "../helper";
 import Subject from "../model/subject";
 import {SubjectView} from "./subjectView";
+
+const style = {
+    paper: {
+        margin: "auto",
+        width: "50%",
+    },
+};
 
 export interface ISubjectListViewProps {
     subjects: Subject[];
@@ -10,12 +18,14 @@ export interface ISubjectListViewProps {
 export const SubjectListView = (props: ISubjectListViewProps) => {
     const subjects = props
         .subjects
-        .map((s) => (<SubjectView
-            subjectName={Beautify(s.Name)}
-            subjectCode={s.Code + " [" + GetInitial(s.Name) + "]"}/>));
+        .map((s) => (
+                <SubjectView
+                    subjectName={Beautify(s.Name)}
+                    subjectCode={s.Code + " [" + GetInitial(s.Name) + "]"} />
+        ));
 
     return (
-        <div>{subjects}
-        </div>
+        <Paper style={style.paper}>{subjects}
+        </Paper>
     );
 };
