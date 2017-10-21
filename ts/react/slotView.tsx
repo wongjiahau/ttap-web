dbb19aacb81e6a603f98e8e8531d6f3587429f5b
WJH <hou32hou@gmail.com>
2017-10-21 13:36:00 +0800

    Updated slotView.tsx

--- ts/react/slotView.tsx
+++ ts/react/slotView.tsx
@@ -1,20 +1,25 @@
 import * as React from "react";
+import {GetInitial} from "../helper";
 import RawSlot from "../model/rawSlot";
 
+const divStyle : React.CSSProperties = {
+    background : "yellow"
+};
+
 export interface ISlotViewProps {
-    slot: RawSlot;
+    slot : RawSlot;
 }
 
-// 'HelloProps' describes the shape of props. State is never set so we use the
-// '{}' type.
 export class SlotView extends React.Component < ISlotViewProps, {} > {
     public render() {
         const slot = this.props.slot;
         return (
-            <div>
-                <h1>{slot.SubjectName}</h1>
-                <h2>{slot.Room}</h2>
-                <h2>{slot.WeekNumber}</h2>
+            <div style={divStyle}>
+                {GetInitial(slot.SubjectName)}
+                <br/>
+                {slot.Room}
+                <br/>
+                {slot.WeekNumber}
             </div>
         );
     }
