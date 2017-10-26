5879f4ba2a5f02e28ae9477eb717a2633704610f
WJH <hou32hou@gmail.com>
2017-10-26 15:39:15 +0800

    Fix all failing test for toggleSubjectListViewingOption

--- ts/redux/tests/toggleSubjectListViewingOption.test.ts
+++ ts/redux/tests/toggleSubjectListViewingOption.test.ts
@@ -3,43 +3,41 @@ import {isEqual} from "lodash";
 import {GetTestSubjects1} from "../../tests/testDataGenerator";
 import {SelectSubject} from "./../actions/selectSubject";
 import {ToggleSubjectListViewingOptions} from "./../actions/toggleSubjectListViewingOption";
-import {ISubjectListState, SubjectListReducer, SubjectListViewState} from "./../reducers/subjectListState";
+import {ISubjectListState, SubjectListReducer, SubjectListState} from "./../reducers/subjectListState";
 
 describe("ToggleSubjectListViewingOption", () => {
+
+    it("the initial value of IsShowingSelectedSubject should be false", () => {
+        const initialState = new SubjectListState();
+        expect(initialState.IsShowingSelectedSubjectOnly).to.eq(false);
+    });
+
     it("should toggle property IsShowingSelectedSubjectOnly from false to true", () => {
-        const initialState = new SubjectListViewState();
+        const initialState = new SubjectListState();
         const newState = SubjectListReducer(initialState, new ToggleSubjectListViewingOptions().Action());
-        const expectedState : ISubjectListState = {
-            ...initialState,
-            IsShowingSelectedSubjectOnly: true
-        };
-        expect(isEqual(newState, expectedState))
+        expect(newState.IsShowingSelectedSubjectOnly)
             .to
             .eq(true);
     });
 
     it("should toggle property IsShowingSelectedSubjectOnly from true to false", () => {
-        const initialState = new SubjectListViewState();
+        const initialState = new SubjectListState();
         const newState1 = SubjectListReducer(initialState, new ToggleSubjectListViewingOptions().Action());
         const newState2 = SubjectListReducer(initialState, new ToggleSubjectListViewingOptions().Action());
-        const expectedState : ISubjectListState = {
-            ...initialState,
-            IsShowingSelectedSubjectOnly: false
-        };
-        expect(isEqual(newState1, expectedState))
+        expect(newState2.IsShowingSelectedSubjectOnly)
             .to
-            .eq(false);
+            .eq(true);
     });
 
     it("should make selected subject visible and deselected subject invisible when first toggled", () => {
-        const initialState = new SubjectListViewState(GetTestSubjects1());
+        const initialState = new SubjectListState(GetTestSubjects1());
         let newState = SubjectListReducer(initialState, new SelectSubject("MPU3113").Action());
         newState = SubjectListReducer(newState, new ToggleSubjectListViewingOptions().Action());
         expect(newState.Subjects.filter((s) => s.IsVisible)).to.have.lengthOf(1);
     });
 
     it("should make all subjects visible when toggled again", () => {
-        const initialState = new SubjectListViewState(GetTestSubjects1());
+        const initialState = new SubjectListState(GetTestSubjects1());
         let newState = SubjectListReducer(initialState, new SelectSubject("MPU3113").Action());
         newState = SubjectListReducer(newState, new ToggleSubjectListViewingOptions().Action());
         newState = SubjectListReducer(newState, new ToggleSubjectListViewingOptions().Action());
