a3329f27faf05fc5f6434fef94976eea01cd9ecd
WJH <hou32hou@gmail.com>
2017-10-29 20:41:52 +0800

    Added ASCII art for transpile.sh lol, i'm just too boring XD

--- scripts/transpile.sh
+++ scripts/transpile.sh
@@ -3,9 +3,12 @@
 clear 
 
 # Clean the output directory
+echo "Cleaning output directory . . ."
 rm -r src/core*
 
 # Transpile TS into JS
+ts_compiler_version=$(tsc -v)
+echo "Transpling TypeScript files into JavaScript (using TSCompiler $ts_compiler_version ). . . "
 output=$(tsc -p ./ts/tsconfig.json)
 
 # Check if output contain error
@@ -14,21 +17,20 @@ then
     echo -e "\e[31mBuild failed due to the following errors: "
     echo "$output"
 else # Signify success
-    ts_compiler_version=$(tsc -v)
-    echo "============================================================"
-    echo -e "\e[32mSuccessfully transpiled TS into JS"
-    echo -e "\e[39mUsing TypeScript compiler $ts_compiler_version"
-    echo " "
-    echo -e "\e[39mThe output files are located at:"
-    echo -e "\e[35m    src/core"
-    echo " "
-    echo -e "\e[39mTo run unit test, type: "
-    echo -e "\e[33m    npm run test"
-    echo " "
-    echo -e "\e[39mTo edit this build output, edit the following file"
-    echo -e "\e[34m    transpile.sh"
+    echo -e "\e[32m "
+    echo "███████╗ ██╗   ██╗  ██████╗  ██████╗ ███████╗ ███████╗ ███████╗ ██╗"
+    echo "██╔════╝ ██║   ██║ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝ ██║"
+    echo "███████╗ ██║   ██║ ██║      ██║      █████╗   ███████╗ ███████╗ ██║"
+    echo "╚════██║ ██║   ██║ ██║      ██║      ██╔══╝   ╚════██║ ╚════██║ ╚═╝"
+    echo "███████║ ╚██████╔╝ ╚██████╗ ╚██████╗ ███████╗ ███████║ ███████║ ██╗"
+    echo "╚══════╝  ╚═════╝   ╚═════╝  ╚═════╝ ╚══════╝ ╚══════╝ ╚══════╝ ╚═╝"
+    echo -e "\e[39mThe output files are located at: \e[35m src/core"
+    echo -e "\e[39mTo run unit test, type: \e[33m npm run test"
+    echo -e "\e[39mTo edit the build output, edit \e[34m transpile.sh"
     echo " "
 fi
 
+
 # Refer the following link for colors and formatting in bash
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
