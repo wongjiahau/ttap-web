080b28cdc8b6e72ad489761adb71e2182135196b
WJH <hou32hou@gmail.com>
2017-10-30 14:17:05 +0800

    Now state-generator will automatically update allReducer.ts to add the new reducer

--- scripts/generate-state.js
+++ scripts/generate-state.js
@@ -41,10 +41,40 @@ export const ${stateName}Reducer = GenereteReducer(new ${stateName}());
 const fileName = targetDir + decapitalized(stateName) + ".ts";
 
 fs.writeFile(fileName, snippet, function(err) {
-    if(err) {
+    if (err) {
         return console.log(err);
     }
     shell.exec(`code ${fileName}`);
     console.log(`${fileName} is opened in VSCode`);
-}); 
+});
 
+// append the new reducer to allReducer.ts
+const allReducerFileName = __dirname + "/../ts/redux/reducers/allReducers.ts";
+fs.readFile(allReducerFileName, (err, content) => {
+    if (err) {
+        return console.log(err);
+    }
+    const allReducerCode = content.toString();
+    let result = insertCodeAt(allReducerCode, "// new-import",
+        `import {${stateName}Reducer} from "./${decapitalized(stateName)}";
+`
+    );
+    result = insertCodeAt(result, "// new-reducer",
+        `${stateName}Reducer,
+`
+    );
+    fs.writeFile(allReducerFileName, result, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        shell.exec(`code ${allReducerFileName}`);
+        console.log(`allReducer.ts is appended with new reducer!`);
+    });
+});
+
+function insertCodeAt(parent, atWhere, newChild) {
+    const parts = parent.split(atWhere);
+    const head = parts[0];
+    const leg = parts[1];
+    return head + newChild + atWhere + leg;
+}
--- ts/redux/reducers/allReducers.ts
+++ ts/redux/reducers/allReducers.ts
@@ -1,12 +1,16 @@
+// This file is generated,
+// please don't modify it unless you know what you're doing
 import {combineReducers} from "redux";
 import {MainFrameReducer} from "./mainFrameState";
 import {SubjectListReducer} from "./subjectListState";
 import {TimetableCreatorReducer} from "./timetableCreatorState";
 import { TimetableListReducer } from "./timetableListState";
+// new-import
 
 export const AllReducers = combineReducers({
     MainFrameReducer,
     SubjectListReducer,
     TimetableCreatorReducer,
-    TimetableListReducer
+    TimetableListReducer,
+    // new-reducer
 });
