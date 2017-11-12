223786df750a61e39e40fa268416cf1e3baddb1e
WJH <hou32hou@gmail.com>
2017-11-12 14:47:40 +0800

    Completed code and pass test for Defilter()

--- ts/model/states/defilter.ts
+++ ts/model/states/defilter.ts
@@ -1,16 +1,19 @@
 import { Timetable } from "../timetable";
 import { State, StateKind } from "./state";
 
-export function Defilter(timetables: Timetable[], state: State): Timetable[] {
+export function Defilter(residueTimetables: Timetable[], state: State): [Timetable[], Timetable[]] {
     if (state.Kind !== StateKind.Clicked) {
         throw new Error("Only state that is Clicked can call the Defilter function");
     }
-    const result = [];
-    for (let i = 0; i < timetables.length; i++) {
-        const t = timetables[i];
-        if ((t.State[state.Day] & state.TimePeriod) === 0) {
-            result.push(t);
+    const rescuedTimetables = [];
+    const unrescudeTimetables = [];
+    for (let i = 0; i < residueTimetables.length; i++) {
+        const t = residueTimetables[i];
+        if ((t.State[state.Day] & state.TimePeriod) > 0) {
+            rescuedTimetables.push(t);
+        } else {
+            unrescudeTimetables.push(t);
         }
     }
-    return result;
+    return [rescuedTimetables, unrescudeTimetables];
 }
--- ts/model/states/tests/_defilter.test.ts
+++ ts/model/states/tests/_defilter.test.ts
@@ -1,9 +1,13 @@
 import { expect } from "chai";
 import { Defilter } from "../defilter";
+import { Filter } from "../filter";
 import { GetTestTimetables1 } from "./../../../tests/testDataGenerator";
 import { State, StateKind } from "./../state";
 
-const state = new State(StateKind.MaybeOccupied, 0, 16, null);
+const state1 = new State(StateKind.MaybeOccupied, 0, 16, null); // Monday 10.00 am to 10.30 am
+const state2 = new State(StateKind.Clicked, 0, 16, null); // Monday 10.00 am to 10.30 am
+const state3 = new State(StateKind.MaybeOccupied, 2, 16, null); // Wednesday 10.00 am to 10.30 am
+const state4 = new State(StateKind.Clicked, 2, 16, null); // Wednesday 10.00 am to 10.30 am
 const timetables = GetTestTimetables1();
 
 describe("Defilter()", () => {
@@ -14,9 +18,26 @@ describe("Defilter()", () => {
         expect(() => {Defilter(timetables, new State(StateKind.Clicked, null, null, null)); }).to.not.throw();
     });
 
-    it("case 1", () => {
-        // TODO: Complete code here
+    it("should return a tuple : [rescuedTimetables, unrescuedTimetables]", () => {
+        const result = Defilter(timetables, state2);
+        expect(result.length).to.eq(2);
+    });
 
+    it("case 1", () => {
+        const [filtrate, residue] = Filter(timetables, state1);
+        const [rescuedTimetables, unrescuedTimetables] = Defilter(residue, state2);
+        expect(rescuedTimetables.length).to.eq(residue.length);
+        expect(unrescuedTimetables.length).to.eq(0);
+    });
+
+    it("case 2", () => {
+        const [filtrate1, residue1] = Filter(timetables, state1);
+        const [filtrate2, residue2] = Filter(filtrate1, state3);
+        const totalResidue = residue1.concat(residue2);
+        expect(totalResidue.length).to.eq(13);
+        const [rescuedTimetables, unrescuedTimetables] = Defilter(totalResidue, state2);
+        expect(rescuedTimetables.length).to.eq(5);
+        expect(unrescuedTimetables.length).to.eq(8);
     });
 
 });
