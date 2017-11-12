857625c688e9fd36f79e2ab7c2c4c8fe76ac037b
WJH <hou32hou@gmail.com>
2017-11-12 14:22:37 +0800

    Passed test for Filter()

--- ts/model/states/tests/_filter.test.ts
+++ ts/model/states/tests/_filter.test.ts
@@ -3,7 +3,8 @@ import { Filter } from "../filter";
 import { GetTestTimetables1 } from "./../../../tests/testDataGenerator";
 import { State, StateKind } from "./../state";
 
-const state = new State(StateKind.MaybeOccupied, 0, 16, null);
+const state1 = new State(StateKind.MaybeOccupied, 0, 16, null); // Monday 10.00 am to 10.30 am
+const state2 = new State(StateKind.MaybeOccupied, 2, 16, null); // Wednesday 10.00 am to 10.30 am
 const timetables = GetTestTimetables1();
 
 describe("Filter()", () => {
@@ -14,11 +15,21 @@ describe("Filter()", () => {
         expect(() => {Filter(timetables, new State(StateKind.MaybeOccupied, null, null, null)); }).to.not.throw();
     });
 
-    
-    it('case 1', () => {
-        // TODO: Complete code here
-        
+    it("should return a tuple : [filtrate, residue]", () => {
+        const result = Filter(timetables, state1);
+        expect(result.length).to.eq(2);
     });
-    
 
+    it("case 1", () => {
+        const [filtrate, residue] = Filter(timetables, state1);
+        expect(filtrate.length).to.eq(24);
+        expect(residue.length).to.eq(5);
+    });
+
+    it("case 2", () => {
+        const [filtrate1, residue1] = Filter(timetables, state1);
+        const [filtrate2, residue2] = Filter(filtrate1, state2);
+        expect(filtrate2.length).to.eq(16);
+        expect(residue1.length + residue2.length).to.eq(13);
+    });
 });
