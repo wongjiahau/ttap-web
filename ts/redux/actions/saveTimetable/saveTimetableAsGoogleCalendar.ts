ba5e7bc8b33ad46cfc96a27eca916bb9ce1ac144
WJH <hou32hou@gmail.com>
2017-11-15 20:53:14 +0800

    Added CreateEvent function for SaveTimetableAsGoogleCalendar

--- ts/redux/actions/saveTimetable/saveTimetableAsGoogleCalendar.ts
+++ ts/redux/actions/saveTimetable/saveTimetableAsGoogleCalendar.ts
@@ -1,19 +1,43 @@
 /* global gapi */
-declare var gapi : any;
-import {Timetable} from "../../../model/timetable";
-import {SaveTimetable} from "./saveTimetable";
+declare let gapi: any;
+import * as moment from "moment";
+import {
+    Time
+} from "../../../att/time";
+import {
+    Beautify
+} from "../../../helper";
+import {
+    RawSlot
+} from "../../../model/rawSlot";
+import {
+    Timetable
+} from "../../../model/timetable";
+import {
+    TimePeriod
+} from "./../../../att/timePeriod";
+import {
+    Week
+} from "./../../../att/week";
+import {
+    SaveTimetable
+} from "./saveTimetable";
 
 export class SaveTimetableAsGoogleCalendar extends SaveTimetable {
     private loginAlready = false;
+    private rawSlots: RawSlot[];
     private CLIENT_ID = "747215402741-95mierrdvdvh2ap8vn5jdk2i0u2totia.apps.googleusercontent.com";
     // TODO: Encrypt this
     private API_KEY = "AIzaSyAoizTbHtbJAyEtwumZDvNd_-DvybawDtw";
     private DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
     private SCOPES = "https://www.googleapis.com/auth/calendar";
-    protected Save(timetable : Timetable) {
+    protected Save(timetable: Timetable) {
+        this.rawSlots = RawSlot.GetBunch(timetable.HashIds);
         this.loadClient();
     }
-    protected SaveType() : string {return "google calendar";}
+    protected SaveType(): string {
+        return "google calendar";
+    }
 
     private loadClient = () => {
         // alert(window.location.href);
@@ -24,7 +48,12 @@ export class SaveTimetableAsGoogleCalendar extends SaveTimetable {
     private initClient = () => {
         gapi // eslint-disable-line
             .client
-            .init({apiKey: this.API_KEY, clientId: this.CLIENT_ID, discoveryDocs: this.DISCOVERY_DOCS, scope: this.SCOPES})
+            .init({
+                apiKey: this.API_KEY,
+                clientId: this.CLIENT_ID,
+                discoveryDocs: this.DISCOVERY_DOCS,
+                scope: this.SCOPES
+            })
             .then(() => {
                 console.log("initClient() success");
                 // Listen for sign-in state changes.
@@ -41,9 +70,9 @@ export class SaveTimetableAsGoogleCalendar extends SaveTimetable {
 
     private updateSigninStatus = (isSignedIn) => {
         console.log("updateSigninState() where isSignedIn = " + isSignedIn);
-        if (isSignedIn) {
+        if (!this.loginAlready && isSignedIn) {
             // this.listUpcomingEvents();
-            this.addEvents();
+            this.addTimetable();
             this.handleSignoutClick();
             this.loginAlready = true;
         } else {
@@ -53,27 +82,26 @@ export class SaveTimetableAsGoogleCalendar extends SaveTimetable {
         }
     }
 
-    private addEvents() {
+    private addTimetable() {
+        this.rawSlots.forEach((s) => {
+            this.addEvents(s);
+        });
+        this.addWeekNumberHeader();
+    }
+
+    private addWeekNumberHeader() {
+        // TODO: Add week number header for each week
+    }
+
+    private addEvents(slot: RawSlot) {
+        const calenderEvent = CreateEvent(slot, new Date(2017, 10, 15));
         gapi // eslint-disable-line
             .client
             .calendar
             .events
             .insert({
                 calendarId: "primary",
-                resource: {
-                    summary: "test event",
-                    location: "test location",
-                    description: "test description",
-                    start: {
-                        dateTime: "2017-11-15T03:30:00+08:00", // +08:00 means UTC+08:00
-
-                        timeZone: "UTC+08:00"
-                    },
-                    end: {
-                        dateTime: "2017-11-15T04:30:00+08:00",
-                        timeZone: "UTC+08:00"
-                    }
-                }
+                resource: calenderEvent
             })
             .execute((event) => {
                 // TODO: Implement snackbar confirmation.
@@ -125,4 +153,98 @@ export class SaveTimetableAsGoogleCalendar extends SaveTimetable {
                 }
             });
     }
+}
+
+function sampleAddEvent() {
+    gapi // eslint-disable-line
+        .client
+        .calendar
+        .events
+        .insert({
+            calendarId: "primary",
+            resource: {
+                summary: "test event",
+                location: "test location",
+                description: "test description",
+                start: {
+                    dateTime: "2017-11-15T03:30:00+08:00", // +08:00 means UTC+08:00
+
+                    timeZone: "UTC+08:00"
+                },
+                end: {
+                    dateTime: "2017-11-15T05:30:00+08:00",
+                    timeZone: "UTC+08:00"
+                },
+                // Recur on 2017-11-22 3:30 am, 2017-11-29 3:30 am
+                recurrence: [`RDATE;TZID=Asia/Kuala_Lumpur:20171122T033000,20171129T033000`]
+            }
+        })
+        .execute((event) => {
+            // TODO: Implement snackbar confirmation.
+        });
+
+}
+
+export function CreateEvent(slot: RawSlot, semesterStartDate: Date) {
+    const t = TimePeriod.Parse(slot.TimePeriod);
+    const w = Week.Parse(slot.WeekNumber);
+    semesterStartDate.setHours(t.StartTime.Hour);
+    semesterStartDate.setMinutes(t.StartTime.Minute);
+    const dates = GetListOfDates(semesterStartDate, w.WeekNumberList);
+    const startDate = moment(dates[0]).format("YYYY-MM-DD");
+    const recurrence = GetRecurrence(dates.slice(1));
+    const event = {
+        summary: `${Beautify(slot.SubjectName)} (${slot.Type}-${slot.Group})`,
+        location: slot.Room,
+        description: `Subject code : ${slot.SubjectCode}, Week : ${slot.WeekNumber}`,
+        start: {
+            // dateTime: "2017-11-15T03:30:00+08:00", // +08:00 means UTC+08:00
+            dateTime: startDate + "T" + t.GetStartTimeInIsoFormat() + ":00+08:00",
+            timeZone: "UTC+08:00"
+        },
+        end: {
+            dateTime: startDate + "T" + t.GetEndTimeInIsoFormat() + ":00+08:00",
+            timeZone: "UTC+08:00"
+        },
+        recurrence: [recurrence]
+    };
+    return event;
+}
+
+export function AddByWeek(date: Date, numberOfWeeks: number): Date {
+    const numberOfDaysPerWeek = 7;
+    const clone = new Date(date.getTime());
+    clone.setDate(clone.getDate() + numberOfWeeks * 7);
+    return clone;
+}
+
+export function GetRecurrence(dates: Date[]): string {
+    let header = "RDATE;TZID=Asia/Kuala_Lumpur:";
+    dates.forEach((x) => {
+        header += ToPureIsoString(x) + ",";
+    });
+    return header.substring(0, header.length - 1);
+}
+
+export function GetListOfDates(startDate: Date, week: number[]): Date[] {
+    const result: Date[] = [];
+    week.forEach((x) => {
+        result.push(AddByWeek(startDate, x));
+    });
+    return result;
+}
+
+export function ToPureIsoString(date: Date): string {
+    function pad(input: number): string {
+        if (input < 10) {
+            return "0" + input;
+        }
+        return input.toString();
+    }
+    return date.getFullYear() +
+        pad(date.getMonth() + 1) +
+        pad(date.getDate()) + "T" +
+        pad(date.getHours()) +
+        pad(date.getMinutes()) +
+        pad(date.getSeconds());
 }
--- ts/redux/actions/saveTimetable/tests/_saveTimetableAsGoogleCalender.test.ts
+++ ts/redux/actions/saveTimetable/tests/_saveTimetableAsGoogleCalender.test.ts
@@ -2,32 +2,117 @@ import {
     expect
 } from "chai";
 import {
-    SaveTimetableAsGoogleCalendar
-} from "./../saveTimetableAsGoogleCalendar";
+    AddByWeek,
+    CreateEvent,
+    GetListOfDates,
+    GetRecurrence,
+    ToPureIsoString
+} from "../saveTimetableAsGoogleCalendar";
+import {
+    GetTestRawSlot1
+} from "./../../../../tests/testDataGenerator";
 
 describe("SaveTimetableAsGoogleCalendar", () => {
-    describe("GetStartDate", () => {
+    describe("AddByWeek", () => {
         it("case 1", () => {
-            const semStartDate = new Date(2017, 10, 15); // 2017 November 15 (because month is represented from 0 - 11)
-            const firstWeek = 0;
-            const result = SaveTimetableAsGoogleCalendar.GetStartDate(semStartDate, firstWeek);
-            expect(result.getTime()).to.eq(new Date(2017, 10, 15).getTime());
+            const input = new Date(2017, 10, 15);
+            const numberOfWeekToBeAdded = 2;
+            const result = AddByWeek(input, numberOfWeekToBeAdded);
+            expect(result.getTime()).to.eq(new Date(2017, 10, 29).getTime());
         });
 
         it("case 2", () => {
-            const semStartDate = new Date(2017, 10, 15); // 2017 November 15 (because month is represented from 0 - 11)
-            const firstWeek = 1;
-            const result = SaveTimetableAsGoogleCalendar.GetStartDate(semStartDate, firstWeek);
-            expect(result.getTime()).to.eq(new Date(2017, 10, 22).getTime());
-        });
-
-        it("case 3", () => {
-            const semStartDate = new Date(2017, 10, 15); // 2017 November 15 (because month is represented from 0 - 11)
-            const firstWeek = 3;
-            const result = SaveTimetableAsGoogleCalendar.GetStartDate(semStartDate, firstWeek);
-            expect(result.getTime()).to.eq(new Date(2017, 11, 6).getTime());
+            const input = new Date(2017, 10, 15);
+            const numberOfWeekToBeAdded = 0;
+            const result = AddByWeek(input, numberOfWeekToBeAdded);
+            expect(result.getTime()).to.eq(new Date(2017, 10, 15).getTime());
         });
 
     });
 
+    describe("ToPureIsoString", () => {
+        it("case 1", () => {
+            const input = new Date(2017, 10, 15, 3, 30);
+            const result = ToPureIsoString(input);
+            expect(result).to.eq("20171115T033000");
+        });
+
+        it("case 2", () => {
+            const input = new Date(2017, 11, 31, 13, 30);
+            const result = ToPureIsoString(input);
+            expect(result).to.eq("20171231T133000");
+        });
+    });
+
+    describe("GetListOfDate", () => {
+        it("case 1", () => {
+            const startDate = new Date(2017, 10, 15, 3, 30);
+            const week = [0, 1, 2];
+            const expected = [
+                new Date(2017, 10, 15, 3, 30),
+                new Date(2017, 10, 22, 3, 30),
+                new Date(2017, 10, 29, 3, 30),
+            ];
+            const actual = GetListOfDates(startDate, week);
+            for (let i = 0; i < week.length; i++) {
+                expect(actual[i].getTime()).to.eq(expected[i].getTime());
+            }
+        });
+
+        it("case 2", () => {
+            const startDate = new Date(2017, 10, 15, 3, 30);
+            const week = [2, 7];
+            const expected = [
+                new Date(2017, 10, 29, 3, 30),
+                new Date(2018, 0, 3, 3, 30),
+            ];
+            const actual = GetListOfDates(startDate, week);
+            for (let i = 0; i < week.length; i++) {
+                expect(actual[i].getTime()).to.eq(expected[i].getTime());
+            }
+        });
+    });
+
+    describe("GetRecurrence", () => {
+        it("case 1", () => {
+            const input = [
+                new Date(2017, 10, 15, 3, 30),
+            ];
+            const result = GetRecurrence(input);
+            expect(result).to.eq("RDATE;TZID=Asia/Kuala_Lumpur:20171115T033000");
+        });
+
+        it("case 2", () => {
+            const input = [
+                new Date(2017, 10, 15, 3, 30),
+                new Date(2017, 10, 22, 3, 30)
+            ];
+            const result = GetRecurrence(input);
+            expect(result).to.eq("RDATE;TZID=Asia/Kuala_Lumpur:20171115T033000,20171122T033000");
+        });
+
+    });
+
+    describe("CreateEvent", () => {
+        it("case 1", () => {
+            const rawSlots = GetTestRawSlot1()[0];
+            const semesterStartDate = new Date(2017, 10, 15);
+            const result = CreateEvent(rawSlots, semesterStartDate);
+            const expected = {
+                summary: "Hubungan Etnik (for Local Students) (L-1)",
+                location: "KB207",
+                description: "Subject code : MPU3113, Week : 1-7",
+                start: {
+                    dateTime: "2017-11-22T08:00:00+08:00",
+                    timeZone: "UTC+08:00"
+                },
+                end: {
+                    dateTime: "2017-11-22T11:00:00+08:00",
+                    timeZone: "UTC+08:00"
+                },
+                recurrence: ["RDATE;TZID=Asia/Kuala_Lumpur:20171129T080000,20171206T080000,20171213T080000,20171220T080000,20171227T080000,20180103T080000"]
+            };
+            expect(result).to.deep.equal(expected);
+        });
+    });
 });
