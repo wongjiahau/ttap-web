73d7b558f0893cfbbb198e7413d638c5c2ae3b13
WJH <hou32hou@gmail.com>
2017-11-16 16:32:03 +0800

    Remove unnecessary code in playground.tsx

--- ts/react/playground.tsx
+++ ts/react/playground.tsx
@@ -17,11 +17,7 @@ export interface IPlaygroundProps {
     id : number; // this line is just to surpress TSLint error on empty interface
 }
 
-export class Playground extends React.Component < IPlaygroundProps, {} > {
-    private CLIENT_ID = "747215402741-95mierrdvdvh2ap8vn5jdk2i0u2totia.apps.googleusercontent.com";
-    private API_KEY = "AIzaSyAoizTbHtbJAyEtwumZDvNd_-DvybawDtw";
-    private DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-    private SCOPES = "https://www.googleapis.com/auth/calendar";
+export class Playground extends React.Component < IPlaygroundProps, {} > {   
     public render() {
         const debugging = false;
         const content = debugging
