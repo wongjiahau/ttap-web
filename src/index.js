f9d869da4a3ad0f9a7429f969342c2166ba849ed
WJH <hou32hou@gmail.com>
2017-11-20 12:43:40 +0800

    Resolved issue #19

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import './index.css';
+import 'typeface-roboto';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {applyMiddleware, createStore} from 'redux';
-import {Provider} from 'react-redux';
+import logger from 'redux-logger'
+import registerServiceWorker from './registerServiceWorker';
 import {AllReducers} from './core/redux/reducers/allReducers';
 import {App} from './App';
 import {Playground} from './core/react/playground';
-import logger from 'redux-logger'
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import 'typeface-roboto';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
 
-let store = createStore(AllReducers, applyMiddleware(logger));
+let store;
+const isProductionBuild = false; // do not modify this line at all unless you know what you're doing, because .travis.yml will modify this line let store
+if (isProductionBuild) {
+  store = createStore(AllReducers)
+} else {
+  store = createStore(AllReducers, applyMiddleware(logger));
+}
 
 ReactDOM.render(
   <Provider store={store}>
