69f9dc63b36bbf3bc049b22a67c2ced493056b7b
WJH <hou32hou@gmail.com>
2017-11-23 17:17:10 +0800

    Deleted mainFrameView

--- ts/redux/reducers/allReducers.ts
+++ ts/redux/reducers/allReducers.ts
@@ -1,12 +1,10 @@
 // This file is generated,
 // please don't modify it unless you know what you're doing
 import {combineReducers} from "redux";
-import {MainFrameReducer} from "./mainFrameState";
 import {TimetableCreatorStateReducer} from "./timetableCreatorState";
 // new-import
 
 export const AllReducers = combineReducers({
-    MainFrameReducer,
     TimetableCreatorStateReducer,
     // new-reducer
 });
