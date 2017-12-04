5a33c37f659847421605e29ff845cd767c2183ef
WJH <hou32hou@gmail.com>
2017-12-04 10:32:54 +0800

    Pass test for ToggleSelectionOnSpecificSlot

--- ts/redux/actions/toggleSelectionOnSpecificSlot.ts
+++ ts/redux/actions/toggleSelectionOnSpecificSlot.ts
@@ -18,58 +18,27 @@ import {
 } from "./toggleSubjectSelection";
 
 export class ToggleSelectionOnSpecificSlot extends MasterStateAction {
-    public constructor(private slotId: number, private checked: boolean, private subjectCode: string) {
+    public constructor(private slotNumber: number, private checked: boolean, private subjectCode: string) {
         super();
     }
     public TypeName(): string {
-        const slotIdsOfRelatedSlots = RawSlot.GetRelated(this.slotId);
         return this.checked ?
-            `selecting slot (${slotIdsOfRelatedSlots.toString()})` :
-            `deselecting slot (${slotIdsOfRelatedSlots.toString()})`;
+            `selecting slot [ ${this.slotNumber} ]` :
+            `deselecting slot [ ${this.slotNumber} ]`;
 
     }
     protected GenerateNewState(state: IMasterState): IMasterState {
-        const slotIdsOfRelatedSlots = RawSlot.GetRelated(this.slotId);
         const newSlotStates = clone(state.SlotTableState.SlotStates);
-        slotIdsOfRelatedSlots.forEach((id) => {
-            newSlotStates[id] = !this.checked;
-        });
-
+        newSlotStates[this.slotNumber] = !this.checked;
         const targetSubject = find(state.SubjectListState.Subjects, {
             Code: this.subjectCode
         });
-        const newStateOfTargetSubject = GetNewStateOfTargetSubject(targetSubject, newSlotStates);
-        const newSubjectStates = clone(state.SlotTableState.SubjectStates);
-        newSubjectStates[this.subjectCode] = newStateOfTargetSubject;
         return {
             ...state,
             SlotTableState: {
                 ...state.SlotTableState,
                 SlotStates: newSlotStates,
-                SubjectStates: newSubjectStates
             }
         };
     }
-}
-
-function GetNewStateOfTargetSubject(subject: Subject, newSlotStates: boolean[]): Ternary {
-    let someSlotIsSelected = false;
-    let someSlotIsNotSelected = false;
-    subject.SlotIds.forEach((id) => {
-        if (newSlotStates[id] === true) {
-            someSlotIsSelected = true;
-        } else {
-            someSlotIsNotSelected = true;
-        }
-    });
-    if (someSlotIsSelected && someSlotIsNotSelected) {
-        return "intermediate";
-    }
-    if (someSlotIsSelected) {
-        return "true";
-    }
-    if (someSlotIsNotSelected) {
-        return "false";
-    }
-
 }
---
+++ ts/redux/tests/_toggleSelectionOnSpecificSlot.test.ts
@@ -0,0 +1,57 @@
+import { expect } from "chai";
+import { RawSlot } from "../../model/rawSlot";
+import { CodeOf, GetTestSubjects1, IndexOf } from "../../tests/testDataGenerator";
+import { ToggleSelectionOnSpecificSlot } from "../actions/toggleSelectionOnSpecificSlot";
+import { ToggleSubjectSelection } from "../actions/toggleSubjectSelection";
+import { IMasterState, MasterStateReducer, NewMasterState } from "../reducers/masterState";
+import { NewSubjectListState } from "../reducers/subjectListState";
+
+function getInitialState(): IMasterState {
+    RawSlot.Reset();
+    return {
+        ...NewMasterState(),
+        SubjectListState: NewSubjectListState(GetTestSubjects1())
+    };
+}
+
+describe("toggle selection on specific slot", () => {
+    it("'s typename should be 'seleting slot [ 1 ]' when passed in true", () => {
+        getInitialState();
+        const action = new ToggleSelectionOnSpecificSlot(1, true, null);
+        expect(action.TypeName()).to.eq("selecting slot [ 1 ]");
+    });
+
+    it("'s typename should be 'deseleting slot [ 1 ]' when passed in false", () => {
+        getInitialState();
+        const action = new ToggleSelectionOnSpecificSlot(1, false, null);
+        expect(action.TypeName()).to.eq("deselecting slot [ 1 ]");
+    });
+
+    it("should set property of SlotStates(1)", () => {
+        // Given Ali selected subject HE
+        // When Ali deselected a slot of HE with hashId of 0
+        // He shall see that the checkbox of slot is dechecked
+        const initialState = getInitialState();
+        let newState = MasterStateReducer(initialState, new ToggleSubjectSelection(IndexOf.HE));
+        const slotNumberOfFirstSlotOfHubunganEtnik = 1;
+        newState = MasterStateReducer(newState,
+            new ToggleSelectionOnSpecificSlot(slotNumberOfFirstSlotOfHubunganEtnik, true, CodeOf.HE));
+        expect(newState.SlotTableState.SlotStates[slotNumberOfFirstSlotOfHubunganEtnik]).to.eq(false);
+    });
+
+    it("should set property of SlotStates(2)", () => {
+        // Given Ali selected subject HE
+        // When Ali deselected a slot of HE with hashId of 0
+        // And then Ali selected back the same slot
+        // He shall see that the checkbox of slot is checked again
+        const initialState = getInitialState();
+        let newState = MasterStateReducer(initialState, new ToggleSubjectSelection(IndexOf.HE));
+        const slotNumberOfFirstSlotOfHubunganEtnik = 1;
+        newState = MasterStateReducer(newState,
+            new ToggleSelectionOnSpecificSlot(slotNumberOfFirstSlotOfHubunganEtnik, true, CodeOf.HE));
+        newState = MasterStateReducer(newState,
+            new ToggleSelectionOnSpecificSlot(slotNumberOfFirstSlotOfHubunganEtnik, false, CodeOf.HE));
+        expect(newState.SlotTableState.SlotStates[slotNumberOfFirstSlotOfHubunganEtnik]).to.eq(true);
+    });
+
+});
