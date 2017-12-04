7a3e9f458be9f699a6e64f74829f504af3b3db20
WJH <hou32hou@gmail.com>
2017-12-04 10:24:05 +0800

    Use dictionary interface for SlotStates and SubjectStates

---
+++ ts/interfaces/dictionary.ts
@@ -0,0 +1,7 @@
+export interface IStringDicionary<T> {
+    [index: string] : T;
+}
+
+export interface INumberDictionary<T> {
+    [index: number] : T;
+}
--- ts/redux/reducers/slotsTableState.ts
+++ ts/redux/reducers/slotsTableState.ts
@@ -1,15 +1,18 @@
+import { IStringDicionary } from "../../interfaces/dictionary";
 import { Ternary } from "../actions/toggleSubjectSelection";
 
 export interface ISlotsTableState {
-    IsOpen: boolean;
-    SlotStates: boolean[]; // whether a slot is selected or not
-    SubjectStates: {}; // whether all slots of a subject is selected or not
+    ErrorMessages: string[];
+    IsOpen:        boolean;
+    SlotStates:    IStringDicionary<boolean>; // whether a slot is selected or not
+    SubjectStates: IStringDicionary<Ternary>; // whether all slots of a subject is selected or not
 }
 
 export function NewSlotsTableState() : ISlotsTableState {
     return {
-        IsOpen: false,
-        SlotStates: [],
-        SubjectStates: {}
+        IsOpen:        false,
+        SlotStates:    {},
+        SubjectStates: {},
+        ErrorMessages: []
     };
 }
