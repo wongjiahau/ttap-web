463bd40db9b04901d166cd10c7a72d158e50ee96
WJH <hou32hou@gmail.com>
2017-12-07 16:47:31 +0800

    Added findTimetableV2.ts

---
+++ ts/permutator/findTimetableV2.ts
@@ -0,0 +1,62 @@
+import {
+    sortBy
+} from "lodash";
+import {
+    RawSlot
+} from "../model/rawSlot";
+import {
+    GetDefinitelyOccupiedState
+} from "../model/states/generateTotalState";
+import {
+    Timetable
+} from "../model/timetable";
+import {
+    ParseRawSlotToSlot
+} from "../parser/parseRawSlotToSlot";
+import {
+    ParseSlotToTinySlot
+} from "../parser/parseSlotToTinySlot";
+import {
+    FindTimetable
+} from "./findTimetable";
+import {
+    PartitionizeByKey
+} from "./partitionize";
+import {
+    GotIntersection
+} from "./state";
+import {
+    TinySlot
+} from "./tinySlot";
+
+export function FindTimetableV2(input: RawSlot[]): Timetable[] {
+    const result: Timetable[] = [];
+    const partitioned = sortBy(PartitionizeByKey(input, "SubjectCode"), ["length"]);
+    const subjects = new Array < TinySlot[] > ();
+    partitioned.forEach((p) => {
+        subjects.push(ParseSlotToTinySlot(ParseRawSlotToSlot(p)));
+    });
+    let currentSlots = subjects[0];
+    let timetables = FindTimetable(currentSlots);
+    let state = GetDefinitelyOccupiedState(timetables);
+    const last = subjects.length - 1;
+    for (let i = 1; i < subjects.length; i++) {
+        const filtrate = FilterOut(subjects[i], state);
+        currentSlots = currentSlots.concat(filtrate);
+        timetables = FindTimetable(currentSlots);
+        if (i !== last) {
+            state = GetDefinitelyOccupiedState(timetables);
+        }
+    }
+    return timetables;
+}
+
+export function FilterOut(slots: TinySlot[], state: number[]) {
+    const result = [];
+    for (let i = 0; i < slots.length; i++) {
+        if (!GotIntersection(slots[i].State, state)) {
+            result.push(slots[i]);
+        }
+    }
+    return result;
+}
