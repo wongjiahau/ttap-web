f459e6615b19245bdcbc5f223a574e2fd4546f52
WJH <hou32hou@gmail.com>
2017-12-07 19:32:51 +0800

    Added some benchmark test - which would not run on CI

---
+++ ts/permutator/tests/_findTimetable.benchmark.test.ts
@@ -0,0 +1,44 @@
+import {
+    concat,
+    uniq
+} from "lodash";
+import {
+    ParseRawSlotToSlot
+} from "../../parser/parseRawSlotToSlot";
+import {
+    ParseSlotToBigSlot
+} from "../../parser/parseSlotToBigSlot";
+import {
+    ParseSlotToTinySlot
+} from "../../parser/parseSlotToTinySlot";
+import {
+    CodeOf,
+    HENG_2017_APR
+} from "../../tests/testData/HENG_2017_APR";
+import {
+    FindTimetable
+} from "../findTimetable";
+
+// This is to test FindTimetable using serious test case
+const input = concat(
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.H), // Hydrology
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.SA2), // Structural Analysis II
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.HT), // Highway Transportation
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.FM2), // Fluid Mechanic II
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.ITBS), // Introduction To Building Services
+);
+
+const tinySlots = ParseSlotToTinySlot(ParseRawSlotToSlot(input));
+
+describe("FindTimetable", () => {
+    describe("using TinySlot", () => {
+        it("case 1", () => {
+            const isCI = require("is-ci");
+            if (!isCI) {
+                const result = FindTimetable(ParseSlotToTinySlot(ParseRawSlotToSlot(input)));
+            }
+        });
+
+    });
+
+});
---
+++ ts/permutator/tests/_findTimetableV2.test.ts
@@ -0,0 +1,28 @@
+import {
+    concat
+} from "lodash";
+import {
+    CodeOf,
+    HENG_2017_APR
+} from "../../tests/testData/heng_2017_apr";
+import {
+    FindTimetableV2
+} from "../findTimetableV2";
+
+const input = concat(
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.H), // Hydrology
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.SA2), // Structural Analysis II
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.HT), // Highway Transportation
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.FM2), // Fluid Mechanic II
+    HENG_2017_APR.filter((x) => x.SubjectCode === CodeOf.ITBS), // Introduction To Building Services
+);
+
+describe("FindTimetableV2", () => {
+    it("case 1", () => {
+        const isCI = require("is-ci");
+        if (!isCI) {
+            const result = FindTimetableV2(input);
+        }
+    });
+
+});
