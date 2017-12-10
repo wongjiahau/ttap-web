d76853042158f9e74d74cfd5c7348edd67cfaba0
WJH <hou32hou@gmail.com>
2017-12-10 21:42:01 +0800

    Fix test for bigSlot

--- ts/permutator/bigSlot.ts
+++ ts/permutator/bigSlot.ts
@@ -35,12 +35,16 @@ export function GetStateOfBigSlot(s: ISlot): number[] {
     const weekBinary = s.Week.toString(2);
     const state = [0, 0, 0, 0, 0, 0, 0];
     state[s.Day - 1] = s.TimePeriod;
+    const maxNumberOfWeek = 14;
     for (let i = weekBinary.length - 1; i >= 0; i--) {
         if (weekBinary[i] === "1") {
             result = result.concat(state.slice());
         } else {
             result = result.concat([0, 0, 0, 0, 0, 0, 0]);
         }
+    }
+    for (let i = result.length / 7; i < maxNumberOfWeek; i++) {
+        result = result.concat([0, 0, 0, 0, 0, 0, 0]);
     }
     return result;
 }
