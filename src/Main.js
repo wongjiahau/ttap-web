599c5556d605823230b343919bf94eb7541955b5
WJH <hou32hou@gmail.com>
2017-12-12 16:56:17 +0800

    Set the first page to be Tutorial

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {FeedbackForm, ReportBugForm} from './pages/embeddedPage';
 import {Playground} from './core/react/playground';
 import {MasterView} from './core/react/masterView';
+import {Tutorial} from './core/react/tutorial/tutorial';
 import {Switch, Route} from 'react-router-dom';
 import {About, Tospp} from './pages/markdownPages';
 
 export const Main = () => (
     <Switch>
-        <Route exact path='/' component={MasterView}/>
+        <Route exact path='/' component={Tutorial}/>
+        <Route exact path='/play' component={MasterView}/>
         <Route exact path='/feedbackForm' component={FeedbackForm}/>
         <Route exact path='/reportBugForm' component={ReportBugForm}/>
         <Route exact path='/404' component={FourOhFour}/>
