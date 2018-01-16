4600ba645cd623b16270252b4d5ee777b9b15ed2
WJH <hou32hou@gmail.com>
2018-01-16 23:15:36 +0800

    Fix a failing test of App.test.tsx

--- ts/fire.ts
+++ ts/fire.ts
@@ -1,5 +1,8 @@
 declare let firebase: any; // this variable is declared in index.html
 export function updateVisits() {
+    if (typeof firebase === "undefined") {
+        return;
+    }
     const ref = firebase.database().ref("visits"); // eslint-disable-line no-undef
     ref.push().set({
         date: Date.now(),
