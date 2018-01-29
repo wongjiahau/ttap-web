62256b52228a7256d7a58bb09fb597be6e923d15
Wong Jia Hau <hou32hou@gmail.com>
2018-01-29 23:08:46 +0800

    The CurrentChoice of generalizedSlot is defaulted to be random -- the reason is documented in in generalizedSlot.ts as comment

--- ts/model/generalizedSlot.ts
+++ ts/model/generalizedSlot.ts
@@ -1,6 +1,7 @@
 import { IRawSlot, RawSlot } from "./rawSlot";
 
 export interface IGeneralizedSlot {
+    CurrentChoice: number; // zero-based index
     SubjectCode: string;
     SubjectName: string;
     Type: string;
@@ -12,16 +13,28 @@ export interface IGeneralizedSlot {
 }
 
 export function CreateGeneralizedSlot(rawSlot: RawSlot): IGeneralizedSlot {
+    const group = rawSlot.Group.split("/");
     return {
-        SubjectCode: rawSlot.SubjectCode,
-        SubjectName: rawSlot.SubjectName,
-        Type: rawSlot.Type,
-        Group: rawSlot.Group.split("/"),
-        Day: rawSlot.Day,
-        TimePeriod: rawSlot.TimePeriod,
-        WeekNumber: rawSlot.WeekNumber.split("/"),
-        Room: rawSlot.Room.split("/")
+        CurrentChoice: getRandomInt(group.length), // Why random? See below.
+        SubjectCode:   rawSlot.SubjectCode,
+        SubjectName:   rawSlot.SubjectName,
+        Type:          rawSlot.Type,
+        Group:         group,
+        Day:           rawSlot.Day,
+        TimePeriod:    rawSlot.TimePeriod,
+        WeekNumber:    rawSlot.WeekNumber.split("/"),
+        Room:          rawSlot.Room.split("/")
     };
+    function getRandomInt(max) {
+        // This function is copied from MDN
+        // Note: getRandomInt(3) will return 0, 1 or 2
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    /*     The CurrentChoice is set as random because it is assumed that most user
+    will not changed the CurrentChoice due to the fact that they may not even
+    know they can change it, so it is better to display random choices
+    so that a particular slot would not be bid by many students while other slots
+    did not get the bid. */
 }
 
 export function CreateGeneralizedSlots(rawSlots: RawSlot[]): IGeneralizedSlot[] {
