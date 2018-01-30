7168e211319235772c9599ddb0e503950cc13f81
Wong Jia Hau <hou32hou@gmail.com>
2018-01-30 22:49:36 +0800

    Added test for state - now I understand whats happening

--- ts/permutator/tests/_state.test.ts
+++ ts/permutator/tests/_state.test.ts
@@ -41,4 +41,15 @@ describe("Append()", () => {
         expect(isEqual(result, [parseInt("11111111", 2), 0, 0, 0, 0, 0, 0])).to.eq(true);
     });
 
+    it("shold return results which have the length of the first input", () => {
+        const a = [0, 0, 0, 0, 0, 0, 0];
+        const b = [0, 0, 0];
+        const result = Append(a, b);
+        expect(result).to.have.lengthOf(a.length);
+        expect(result).to.deep.eq(a);
+        // Why is this happenning ?
+        // Tips : Because any number bitwise-and with undefined equals = 0
+        // E.g. (8 & undefined) will return 0
+    });
+
 });
