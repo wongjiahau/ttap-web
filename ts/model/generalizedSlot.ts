5802e33b6b328c0e057fa464423ff5c8e05c91e5
Wong Jia Hau <hou32hou@gmail.com>
2018-01-30 11:54:30 +0800

    Added passing test for SelectSlotChoice

--- ts/model/generalizedSlot.ts
+++ ts/model/generalizedSlot.ts
@@ -17,7 +17,7 @@ export function CreateGeneralizedSlot(rawSlot: RawSlot): IGeneralizedSlot {
     const group = rawSlot.Group.split("/");
     return {
         Uid:            rawSlot.HashId,
-        CurrentChoice: getRandomInt(group.length), // Why random? See below.
+        CurrentChoice: 0,
         SubjectCode:   rawSlot.SubjectCode,
         SubjectName:   rawSlot.SubjectName,
         Type:          rawSlot.Type,
@@ -27,16 +27,6 @@ export function CreateGeneralizedSlot(rawSlot: RawSlot): IGeneralizedSlot {
         WeekNumber:    rawSlot.WeekNumber.split("/"),
         Room:          rawSlot.Room.split("/")
     };
-    function getRandomInt(max) {
-        // This function is copied from MDN
-        // Note: getRandomInt(3) will return 0, 1 or 2
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-    /*     The CurrentChoice is set as random because it is assumed that most user
-    will not changed the CurrentChoice due to the fact that they may not even
-    know they can change it, so it is better to display random choices
-    so that a particular slot would not be bid by many students while other slots
-    did not get the bid. */
 }
 
 export function CreateGeneralizedSlots(rawSlots: RawSlot[]): IGeneralizedSlot[] {
---
+++ ts/redux/tests/_selectSlotChoice.test.ts
@@ -0,0 +1,70 @@
+import {expect} from "chai";
+import { RawSlot } from "./../../model/rawSlot";
+import {GetTestTimetables1} from "./../../tests/testDataGenerator";
+import {ToggleSubjectSelection} from "./../actions/toggleSubjectSelection";
+const isEqual = require("lodash.isequal");
+import ParseHtmlToRawSlot from "../../parser/parseHtmlToRawSlot";
+import {ParseSlotToSubject} from "../../parser/parseSlotToSubject";
+import TestManager, {FileName} from "../../tests/testManager";
+import {NewSubjectListState} from "../reducers/subjectListState";
+import {NewTimetableListState} from "../reducers/timetableListState";
+import {SelectSlotChoice} from "./../actions/selectSlotChoice";
+import {IMasterState, MasterStateReducer, NewMasterState} from "./../reducers/masterState";
+
+function getInitialState() : IMasterState {
+    const slots = ParseHtmlToRawSlot(new TestManager().GetDataFrom(FileName.cf_2017_nov));
+    const subjects = ParseSlotToSubject(slots);
+    return {
+        ...NewMasterState(),
+        SubjectListState: NewSubjectListState(subjects)
+    };
+}
+
+describe("SelectSlotChoice action", () => {
+    it("'s typename should be 'select slot choice'", () => {
+        const action = new SelectSlotChoice(null, null);
+        expect(action.TypeName())
+            .to
+            .eq("select slot choice");
+    });
+
+    it("case 1", () => {
+        RawSlot.Reset();
+        const initialState = getInitialState();
+        const indexOfUEMK3233 = 4; // Bioprocess Engineering
+        let newState = MasterStateReducer(initialState, new ToggleSubjectSelection(indexOfUEMK3233));
+        expect(newState.TimetableListState.CurrentSlots[2]).to.deep.eq({
+            Uid: 229,
+            CurrentChoice: 0,
+            SubjectCode: "UEMK3233",
+            SubjectName: "BIOPROCESS ENGINEERING",
+            Type: "T",
+            Group: [
+                "1", "2"
+            ],
+            Day: "Tue",
+            TimePeriod: "02:00 PM - 03:00 PM",
+            WeekNumber: [
+                "3,5,7,9,11,13", "2,4,6,8,10,12"
+            ],
+            Room: ["KB326", "KB326"]
+        });
+        newState = MasterStateReducer(newState, new SelectSlotChoice(229, 1));
+        expect(newState.TimetableListState.CurrentSlots[2]).to.deep.eq({
+            Uid: 229,
+            CurrentChoice: 1,
+            SubjectCode: "UEMK3233",
+            SubjectName: "BIOPROCESS ENGINEERING",
+            Type: "T",
+            Group: [
+                "1", "2"
+            ],
+            Day: "Tue",
+            TimePeriod: "02:00 PM - 03:00 PM",
+            WeekNumber: [
+                "3,5,7,9,11,13", "2,4,6,8,10,12"
+            ],
+            Room: ["KB326", "KB326"]
+        });
+    });
+});
