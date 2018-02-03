de96f214b83b530ac27da59478981ab06e60250a
Wong Jia Hau <hou32hou@gmail.com>
2018-02-03 10:33:26 +0800

    Fix failing test related to goto{which}Timetable

--- ts/redux/tests/_goToRandomTimetable.test.ts
+++ ts/redux/tests/_goToRandomTimetable.test.ts
@@ -1,25 +1,14 @@
-import {
-    expect
-} from "chai";
+import { expect } from "chai";
 const isEqual = require("lodash.isequal");
-import {
-    Timetable
-} from "../../model/timetable";
-import {
-    NewTimetableListState
-} from "../reducers/timetableListState";
-import {
-    GoToRandomTimetable
-} from "./../actions/goToRandomTimetable";
-import {
-    IMasterState,
-    MasterStateReducer,
-    NewMasterState
-} from "./../reducers/masterState";
+import { Timetable } from "../../model/timetable";
+import { GetTestRawSlot1 } from "../../tests/testDataGenerator";
+import { NewTimetableListState } from "../reducers/timetableListState";
+import { GoToRandomTimetable } from "./../actions/goToRandomTimetable";
+import { IMasterState, MasterStateReducer, NewMasterState } from "./../reducers/masterState";
 
 function getInitialState(timetables: Timetable[]): IMasterState {
     const result = NewMasterState();
-    result.TimetableListState = NewTimetableListState(timetables);
+    result.TimetableListState = NewTimetableListState(timetables, GetTestRawSlot1());
     return result;
 }
 
