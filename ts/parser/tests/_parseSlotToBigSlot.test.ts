6fe0a932cc737ef76b4ffaaee05364600b3e6713
Wong Jia Hau <hou32hou@gmail.com>
2018-02-04 15:14:17 +0800

    Added test for parseSlotToBigSlot

--- ts/parser/tests/_parseSlotToBigSlot.test.ts
+++ ts/parser/tests/_parseSlotToBigSlot.test.ts
@@ -1,29 +1,42 @@
-import { expect } from "chai";
-import TestManager, { FileName } from "../../tests/testManager";
-import ParseHtmlToRawSlot from "../parseHtmlToRawSlot";
-import { ParseRawSlotToSlot } from "../parseRawSlotToSlot";
-import { ParseSlotToBigSlot } from "../parseSlotToBigSlot";
+import {
+    expect
+} from "chai";
+import {
+    HENG_2017_APR
+} from "../../tests/testData/heng_2017_apr";
+import {
+    ParseRawSlotToSlot
+} from "../parseRawSlotToSlot";
+import {
+    ParseSlotToBigSlot
+} from "../parseSlotToBigSlot";
 
-const testSlots = ParseHtmlToRawSlot(new TestManager().GetDataFrom(FileName.cf_2017_nov));
+const testSlots = HENG_2017_APR();
 
 describe("ParseSlotToBigSlot", () => {
-    it("this test is currently disabled", () => {
-        console.log("this test is disabled");
+    it("case 1", () => {
+        const rawSlots = testSlots.filter((s) => s.Uid === 166);
+        expect(rawSlots[0].WeekNumber).to.eq("3,9");
+        const bigSlots = ParseSlotToBigSlot(ParseRawSlotToSlot(rawSlots));
+        expect(bigSlots).to.have.lengthOf(1);
+        expect(bigSlots[0].State).to.have.lengthOf(14 * 7);
+        expect(bigSlots[0].State).to.deep.eq(
+            [
+                0,       0, 0, 0, 0, 0, 0, // week 1
+                0,       0, 0, 0, 0, 0, 0, // week 2
+                0, 1032192, 0, 0, 0, 0, 0, // week 3
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0, 1032192, 0, 0, 0, 0, 0, // week 9
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0,
+                0,       0, 0, 0, 0, 0, 0
+            ]
+        );
     });
-    // it("case 1", () => {
-    //     const rawSlots = testSlots.filter((s) => s.Number === "283");
-    //     const bigSlots = ParseSlotToBigSlot(ParseRawSlotToSlot( rawSlots ));
-    //     expect(bigSlots).to.have.lengthOf(1);
-    //     expect(bigSlots[0].State).to.deep.eq([
-    //         0, 0, 0, 0, 0, 0, 0, // week 1
-    //         1008, 0, 0, 0, 0, 0, 0, // week 2
-    //         0, 0, 0, 0, 0, 0, 0, // week 3
-    //         0, 0, 0, 0, 0, 0, 0, // week 4
-    //         0, 0, 0, 0, 0, 0, 0, // week 5
-    //         0, 0, 0, 0, 0, 0, 0, // week 6
-    //         0, 0, 0, 0, 0, 0, 0, // week 7
-    //         1008, 0, 0, 0, 0, 0, 0// week 8
-    //     ]);
-
-    // });
 });
