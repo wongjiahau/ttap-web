f4d3cac3bd32cde4ebfe7af0e4f092992cf10b9e
Wong Jia Hau <hou32hou@gmail.com>
2018-03-27 00:09:37 +0800

    Now Industrial Training  slots will be excluded

--- ts/parser/parseRawSlotToSlot.ts
+++ ts/parser/parseRawSlotToSlot.ts
@@ -5,12 +5,19 @@ import {
     RawSlot
 } from "./../model/rawSlot";
 export function ParseRawSlotToSlot(rawSlots: RawSlot[]): Slot[] {
-    const parsableRawSlots = rawSlots.filter((x) => IsParsable(x));
+    const parsableRawSlots = rawSlots.filter((x) => IsParsable(x) && IsReasonable(x));
     return parsableRawSlots.map((x) => CreateSlotFromRaw(x));
 }
 
 export function IsParsable(rawSlot: RawSlot): boolean {
     if (rawSlot.TimePeriod === undefined) {
+        return false;
+    }
+    return true;
+}
+
+export function IsReasonable(rawSlot: RawSlot): boolean {
+    if (rawSlot.SubjectName.toLowerCase() === "industrial training") {
         return false;
     }
     return true;
--- ts/parser/tests/_parseRawSlotToSlot.test.ts
+++ ts/parser/tests/_parseRawSlotToSlot.test.ts
@@ -8,7 +8,7 @@ import {
     GetTestRawSlot1
 } from "./../../tests/testDataGenerator";
 import {
-    IsParsable, ParseRawSlotToSlot
+    IsParsable, IsReasonable, ParseRawSlotToSlot
 } from "./../parseRawSlotToSlot";
 
 describe("parseRawSlotToSlot", () => {
@@ -32,7 +32,6 @@ describe("parseRawSlotToSlot", () => {
         }).to.not.throw();
     });
 
-
 });
 
 describe("IsParsable", () => {
@@ -50,6 +49,31 @@ describe("IsParsable", () => {
             Room: undefined
         };
         expect(IsParsable(rawSlot)).to.eq(false);
+    });
+
+});
+
+describe("IsReasonable", () => {
+    it("should think that Industrial Training(IT) is not reasonable", () => {
+        /** This is because those who are taking IT will not need to use this app
+         * Why we need to filter out IT slot?
+         * This is because it will make the Min time to be at 7 a.m.
+         * We want the Min time to be as large as possible, so that
+         * the find timetable algorithm can work faster
+         */
+        const rawSlot : IRawSlot =  {
+            Uid: 43,
+            SubjectCode: "No record found!!!",
+            SubjectName: "Industrial Training",
+            Number: "No record found!!!",
+            Type: undefined,
+            Group: undefined,
+            Day: undefined,
+            TimePeriod: undefined,
+            WeekNumber: undefined,
+            Room: undefined
+        };
+        expect(IsReasonable(rawSlot)).to.eq(false);
     });
 
 });
