99859a2e5444e64a7f8ac3e2978eb5dfb2915345
Wong Jia Hau <hou32hou@gmail.com>
2018-04-11 12:57:59 +0800

    Updated testManager

--- ts/tests/testManager.ts
+++ ts/tests/testManager.ts
@@ -15,10 +15,11 @@ function LoadFileFromGitHub(downloadLink: string): string {
 }
 
 export enum FileName {
-    jiahau_2017_sept = "jiahau_2017_sept.html",
-    cf_2017_nov = "cf_2017_nov.html",
-        heng_2017_sept = "heng_2017_sept.html",
-        heng_2017_nov = "heng_2017_nov.html",
-        keli_2017_sept = "keli_2017_sept.html",
-        qingsheng_2017_sept = "qingsheng_2017_sept.html",
+    jiahau_2017_sept    = "jiahau_2017_sept.html",
+    cf_2017_nov         = "cf_2017_nov.html",
+    heng_2017_sept      = "heng_2017_sept.html",
+    heng_2017_nov       = "heng_2017_nov.html",
+    keli_2017_sept      = "keli_2017_sept.html",
+    qingsheng_2017_sept = "qingsheng_2017_sept.html",
+    all_fes_slots       = "fes-2018-05-slots.html"
 }
