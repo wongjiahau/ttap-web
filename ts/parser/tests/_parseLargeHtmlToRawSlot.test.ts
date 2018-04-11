dddb6ab9685ec2a878ca98fad5364a3e8dd16e76
Wong Jia Hau <hou32hou@gmail.com>
2018-04-11 12:57:31 +0800

    Added more test for parseLargeHtmlToRawSlot

--- ts/parser/tests/_parseLargeHtmlToRawSlot.test.ts
+++ ts/parser/tests/_parseLargeHtmlToRawSlot.test.ts
@@ -1,34 +1,15 @@
 import { expect } from "chai";
-const isEqual = require("lodash.isequal");
+const last = require("lodash.last");
 const omit = require("lodash.omit");
-import { RawSlot } from "../../model/rawSlot";
 import TestManager, { FileName } from "../../tests/testManager";
 import { ParseLargeHtmlToRawSlot } from "../parseLargeHtmlToRawSlot";
 import { IRawSlot } from "./../../model/rawSlot";
 
-import ParseHtmlToRawSlot from "../parseHtmlToRawSlot";
-const fs = require("fs");
-const path = require("path");
-
-const tabletojson = require("tabletojson");
-
-function tableToJson(table: HTMLTableElement, headers: string[]): object[] {
-    const data = [];
-    for (let i = 1; i < table.rows.length; i++) {
-        const tableRow = table.rows[i];
-        const rowData = {};
-        for (let j = 0; j < tableRow.cells.length; j++) {
-            rowData[headers[j]] = tableRow.cells[j].innerHTML;
-        }
-        data.push(rowData);
-    }
-    return data;
-}
+const html = new TestManager().GetDataFrom(FileName.all_fes_slots);
+const rawSlots = ParseLargeHtmlToRawSlot(html);
 
 describe("ParseHtmlToRawSlot", () => {
-    it("case 1", (done) => {
-        const html = new TestManager().GetDataFrom(FileName.all_fes_slots);
-        const rawSlots = ParseLargeHtmlToRawSlot(html);
+    it("case 1", () => {
         expect(rawSlots[0]).to.deep.eq({
             Uid:         0,
             SubjectCode: "MPU32033",
@@ -44,39 +25,47 @@ describe("ParseHtmlToRawSlot", () => {
             Room:        "KB316",
             Remark:      ""
         });
-        done();
-        // fs.readFile("../ttap-sample-data/Archive/fes-2018-05-slots.html", (err, data) => {
-            // const htmlDoc = new DOMParser().parseFromString(data, "text/html");
-            // const table = htmlDoc.getElementsByTagName("table")[10];
-            // const headers = "Number,Type,Group,ClassSize,Day,Time,CreditHour,WeekNumber,Room,LECTURER,REG.,AVAIL,RESERVE,Remark".split(",");
-            // const rawSlots = ParseHtmlToRawSlot(table.innerHTML);
-            // const json = tableToJson(table, headers);
-            // for (let i = 0; i < 10; i++) {
-            //     console.log(json[i]["Number"]);
-            //     console.log(Object.keys(json[i]).length);
-            // }
-            // const result : IRawSlot[] = json.map((x, index) => ({
-            //     Uid: index,
-            //     SubjectCode: string;
-            //     SubjectName: string;
-            //     Number: string; // Slot number, which is not necessarily unique for every slot
-            //     Type: string;
-            //     Group: string;
-            //     ClassSize ? : string;
-            //     Day: string;
-            //     TimePeriod: string;
-            //     CreditHour ? : string;
-            //     WeekNumber: string;
-            //     Room: string;
-            //     Remark ? : string;
-            // }));
-            // const tablesAsJson = tabletojson.convert(`<html><body>${tableHTML}</body></html>`);
-            // console.log(tablesAsJson);
-        //     done();
-        // });
-        // console.log(tablesAsJson[2].slice(10));
-        // const result = ParseLargeHtmlToRawSlot(input);
-        // expect(result).to.have.lengthOf(1499);
     });
 
+    it("case 2", () => {
+        expect(last(rawSlots)).to.deep.eq({
+            Uid: 1688,
+            SubjectCode: "UJLL1093",
+            SubjectName: "INTRODUCTION TO KOREAN",
+            Number:      "1311",
+            Type:        "T",
+            Group:       "2",
+            ClassSize:   "20",
+            Day:         "Wed",
+            TimePeriod:  "11:00 AM - 12:00 PM",
+            CreditHour:  "1.0",
+            WeekNumber:  "1-14",
+            Room:        "KB521",
+            Remark:      ""
+        });
+    });
+
+    it("case 3: subslots will not have property of ClassSize and Remark", () => {
+        expect(rawSlots.filter((x) => x.Number === "660")[1]).to.deep.eq({
+            Uid: 799,
+            SubjectCode: "UEEP2623",
+            SubjectName: "MICROELECTRONIC CIRCUIT ANALYSIS",
+            Number:      "660",
+            Type:        "L",
+            Group:       "1",
+            // ClassSize:   "15",
+            Day:         "Tue",
+            TimePeriod:  "08:00 AM - 10:00 AM",
+            CreditHour:  "2.0",
+            WeekNumber:  "1-14",
+            Room:        "KB326",
+            // Remark:      ""
+        });
+    });
+
+    it("case 4", () => {
+        const maxSlotNumber = 1311; // The slot number of the last slot
+        const numberOfSubSlots = 378; // By counting occurence of subRows in the raw HTML
+        expect(rawSlots).to.have.lengthOf(maxSlotNumber + numberOfSubSlots);
+    });
 });
