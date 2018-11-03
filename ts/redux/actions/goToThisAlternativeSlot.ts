f33891b02257df1e601008af5470a0120afbe7b3
Wong Jia Hau <hou32hou@gmail.com>
2018-11-03 15:15:51 +0800

    Added more test for goToThisAlternativeSlot

--- ts/redux/actions/goToThisAlternativeSlot.ts
+++ ts/redux/actions/goToThisAlternativeSlot.ts
@@ -12,21 +12,23 @@ export class GoToThisAlternativeSlot extends MasterStateAction {
             return state;
         }
         const timetables = state.TimetableListState.FiltrateTimetables;
-        const indexOfPossibleDestinations: number[] = [];
+        // tslint:disable-next-line:array-type
+        const indexOfPossibleDestinations: [number, number][] = [];
         for (let i = 0; i < timetables.length; i++) {
             const t = state.TimetableListState.FiltrateTimetables[i];
             for (let j = 0; j < t.ListOfSlotUids.length; j++) {
                 if (t.ListOfSlotUids[j].indexOf(this.destinationSlotUid) > -1) {
-                    indexOfPossibleDestinations.push(i);
+                    indexOfPossibleDestinations.push([i, j]);
                 }
             }
         }
 
         const currentTimetable = timetables[state.TimetableListState.CurrentIndex];
 
-        let indexOfDestinationTimetable = -1; // indexOfPossibleDestinations[0];
+        let indexOfDestinationTimetable: [/*index*/ number, /*subindex*/ number] =
+            [state.TimetableListState.CurrentIndex, state.TimetableListState.CurrentSubIndex];
 
-        const destinationTimetableUids = [...currentTimetable.ListOfSlotUids[0]];
+        const destinationTimetableUids = [...currentTimetable.ListOfSlotUids[state.TimetableListState.CurrentSubIndex]];
         const destinationTimetableSlotNumbers = new Set(
             state.TimetableListState.SlotViewModelStore.GetBunch(
                 destinationTimetableUids
@@ -39,33 +41,28 @@ export class GoToThisAlternativeSlot extends MasterStateAction {
         );
 
         for (let i = 0; i < indexOfPossibleDestinations.length; i++) {
-            const index = indexOfPossibleDestinations[i];
-            const t = timetables[index];
+            const dest = indexOfPossibleDestinations[i];
+            const t = timetables[dest[0 /*index*/]];
+
             if (setEqual(
                     destinationTimetableSlotNumbers,
                     new Set(
                         state.TimetableListState.SlotViewModelStore.GetBunch(
-                            t.ListOfSlotUids[0]
+                        t.ListOfSlotUids[dest[1 /*subindex*/]]
                         ).map((x) => x.SlotNumber)
                     )
                 )) {
-                indexOfDestinationTimetable = index;
+                indexOfDestinationTimetable = dest;
                 break;
             }
-        }
-
-        // in case the indexOfDestinationTimetable can't be found (which is a bug that should be fix)
-        if (indexOfDestinationTimetable < 0) {
-            throw new Error();
-            indexOfDestinationTimetable = indexOfPossibleDestinations[0];
         }
 
         return {
             ...state,
             TimetableListState: {
                 ...state.TimetableListState,
-                CurrentIndex: indexOfDestinationTimetable,
-                CurrentSubIndex: 0,
+                CurrentIndex: indexOfDestinationTimetable[0],
+                CurrentSubIndex: indexOfDestinationTimetable[1],
                 AlternativeSlots: []
             },
             SnackbarState: {
--- ts/redux/tests/_goToThisAlternativeSlot.test.ts
+++ ts/redux/tests/_goToThisAlternativeSlot.test.ts
@@ -63,4 +63,43 @@ describe("showAlternateSlot action", () => {
         expect(currentSlots.some((x) => x.SubjectCode === "UEME3112" && x.Type === "P" && x.Group[0] === "5")).to.eq(true);
     });
 
+    it("case 3", () => {
+        const initialState = GetMockInitialState("heng_2017_apr");
+        const newState0 = MasterStateReducer(initialState, new ToggleSubjectSelection(IndexOf.FM1));
+        const newState1 = MasterStateReducer(newState0, new ToggleSubjectSelection(IndexOf.FM2));
+        const slotsToBeClicked = newState1.TimetableListState.SlotViewModelStore.GetAll()
+            .filter((x) =>
+                x.SubjectCode === "UEME2123" &&
+                x.Type === "P" &&
+                x.Group[0] === "1"
+            )[0]; // FM1 P1 of Tuesday
+        const newState2 = MasterStateReducer(newState1, new FindAlternativeSlotsOfCurrentSlots());
+        const newState3 = MasterStateReducer(newState2, new ShowAlternateSlot(slotsToBeClicked));
+
+        const newState4 = MasterStateReducer(newState3, new GoToThisAlternativeSlot(
+            // Go to FM1-P28 of Friday
+            newState3.TimetableListState.AlternativeSlots.filter((x) => x.Group[0] === "28")[0].Uid)
+        );
+
+        // this check is because if no destination index is found, it will used back the original index
+        expect(newState4.TimetableListState.CurrentIndex).to.not.eq(newState3.TimetableListState.CurrentIndex);
+
+        // changing back to original slots
+        const slotsToBeClicked2 = newState1.TimetableListState.SlotViewModelStore.GetAll()
+            .filter((x) =>
+                x.SubjectCode === "UEME2123" &&
+                x.Type === "P" &&
+                x.Group[0] === "28"
+            )[0]; // FM1 P28 of Friday
+
+        const newState5 = MasterStateReducer(newState4, new FindAlternativeSlotsOfCurrentSlots());
+        const newState6 = MasterStateReducer(newState5, new ShowAlternateSlot(slotsToBeClicked2));
+
+        const newState7 = MasterStateReducer(newState6, new GoToThisAlternativeSlot(
+            // Go to FM1-P1 of Tuesday
+            newState6.TimetableListState.AlternativeSlots.filter((x) => x.Group[0] === "1")[0].Uid)
+        );
+        expect(newState7.TimetableListState.CurrentIndex).to.not.eq(newState6.TimetableListState.CurrentIndex);
+    });
+
 });
